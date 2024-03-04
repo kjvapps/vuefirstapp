c3643addb5b8028c3c0912223a12825a1e885dd7
Kathleen Valdez <72367177+kjvapps@users.noreply.github.com>
2024-03-04 13:32:39 -0800

    adding starting files

---
+++ App.vue
@@ -0,0 +1,33 @@
+<template>
+<section>
+    <h2>My Friends</h2>
+        <ul>
+            <li>
+            </li>
+        </ul>
+   </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [{
+                id: 'manuel',
+                name: 'Manuel Lorenz',
+                phone: '555-555',
+                email: 'manuel@localhost.com'
+            },
+            {
+                id: 'julie',
+                name: 'Julue Jones',
+                phone: '444-555',
+                email: 'julie@localhost.com'
+            }
+            ]
+        }
+    },
+    
+}
+
+</script>
