4c57d761ae43e582414421e1bf6a992330a7268d
Kathleen Valdez <kathleenjval@gmail.com>
2024-03-04 14:14:24 -0800

    adding files

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetals">Show Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone}}</li>
+            <li><strong>Email:</strong>{{ friend.email}}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+
+export default{
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: { id: 'manuel',
+                name: 'Manuel Lorenz',
+                phone: '555-555',
+                email: 'manuel@localhost.com'}
+        }
+    },
+methods: {
+    toggleDetals() {
+        this.detailsAreVisible = !this.detailsAreVisible;
+    }
+}
+}
+   
+</script>
